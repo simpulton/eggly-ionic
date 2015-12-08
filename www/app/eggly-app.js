81bfb271e6073a0a36c50d919c1eacf627e4bd36
Jonathan Garvey <jon.d.garvey@gmail.com>
2015-12-08 10:41:06 -0700

    Remove style block, update dependencies, add in run block

--- www/app/eggly-app.js
+++ www/app/eggly-app.js
@@ -16,4 +16,21 @@ angular.module('Eggly', [
 
         $urlRouterProvider.otherwise('/');
     })
+    .run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+          if(window.cordova && window.cordova.plugins.Keyboard) {
+              // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+              // for form inputs)
+              cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+              // Don't remove this line unless you know what you are doing. It stops the viewport
+              // from snapping when text inputs are focused. Ionic handles this internally for
+              // a mzuch nicer keyboard experience.
+              cordova.plugins.Keyboard.disableScroll(true);
+          }
+          if(window.StatusBar) {
+              StatusBar.styleDefault();
+          }
+        });
+    })
 ;
