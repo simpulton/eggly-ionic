424b25cad4648a1fb28383614de684522c9cde02
Jonathan Garvey <jon.d.garvey@gmail.com>
2015-12-10 11:03:20 -0700

    04-ionic-assets

--- www/app/eggly-app.js
+++ www/app/eggly-app.js
@@ -1,18 +1,32 @@
 angular.module('Eggly', [
-    'ngAnimate',
-    'ui.router',
+    'ionic',
     'categories',
     'categories.bookmarks'
 ])
-    .config(function ($stateProvider, $urlRouterProvider) {
+    .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
-            //abstract state serves as a PLACEHOLDER or NAMESPACE for application states
+        //abstract state serves as a PLACEHOLDER or NAMESPACE for application states
             .state('eggly', {
-                url: '',
-                abstract: true
-            })
-        ;
+            url: '',
+            abstract: true
+        });
 
         $urlRouterProvider.otherwise('/');
     })
-;
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a mzuch nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    });
